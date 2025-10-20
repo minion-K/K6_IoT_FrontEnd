fc2c92bc0104419635322f6885b3463058dd91d4
minion-K <rnjsalsgus06@naver.com>
2025-10-20 12:12:47 +0900

    1020 JS complicit object

---
+++ c_javascript/l_내장객체/obj03.js
@@ -0,0 +1,11 @@
+// 4. Math 내장 객체: 수학과 관련된 기본 연산 객체
+console.log(`원주율 값: ${Math.PI}`);
+
+console.log(`0 이상 1미만의 난수 생성: ${Math.random()}`);
+// console.log(`0 이상 N미만의 난수 생성: ${Math.random() * N}`);
+console.log(`0 이상 10미만의 난수 생성: ${Math.random() * 10}`);
+
+console.log(`소수점 자리 내림: ${Math.floor(Math.random() * 10)}`);
+
+console.log(`제곱근(square root): ${Math.sqrt(16)}`) // 4
+console.log(`제곱근(square root): ${Math.sqrt(225)}`) // 15
