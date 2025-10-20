826107f61081850a55008e019a8b812233ab8af2
minion-K <rnjsalsgus06@naver.com>
2025-10-20 11:24:42 +0900

    1020 JS event practice

---
+++ c_javascript/l_내장객체/obj01.js
@@ -0,0 +1,14 @@
+// ! 자바스크립트 내장 객체
+// : 특정 작업 수행이나 복잡한 기능을 담은 메서드와 속성 제공
+
+// 1. Number 객체의 기본 메서드
+// : 수치형 데이터를 처리하는 속성과 메서드를 포함한 JS 내장 객체
+// > number 자료형
+
+// ! 1) toFixed(N): N자리까지의 반올림
+let num = 123.4567;
+console.log(num.toFixed(3));
+console.log(num.toFixed(1));
+console.log(num.toFixed()); // 자릿수 지정 X - 소수점 전체 반올림 (정수 반환)
+
+// 2. String 객체의 기본 메서드
