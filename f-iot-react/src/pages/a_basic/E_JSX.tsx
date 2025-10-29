0702e355c0c83aa4ffd80a342c45bd7e634bbea5
minion-K <rnjsalsgus06@naver.com>
2025-10-29 16:15:33 +0900

    1029 React JSX 문법

--- f-iot-react/src/pages/a_basic/E_JSX.tsx
+++ f-iot-react/src/pages/a_basic/E_JSX.tsx
@@ -1,9 +1,36 @@
 import React from 'react'
 // https://cdn.pixabay.com/photo/2023/06/29/12/28/cats-8096304_1280.jpg
 
+const cat =  {
+  catUrl: 'https://cdn.pixabay.com/photo/',
+  description: '2023/06/29/12/28/',
+  imageId: 'cats-8096304_1280.jpg',
+  name: '아기고양이',
+  imageTheme: {
+    width: '200px',
+    height: '150px'
+  },
+  theme: {
+    backgroundColor: 'black',
+    color: 'white'
+  }
+}
+
+
+
 function E_JSX() {
   return (
-    <div>E_JSX</div>
+    <>
+      <div style={cat.theme}>
+        <p>{cat.name}'s Picture</p>
+        <img 
+          src={cat.catUrl + cat.description + cat.imageId} 
+          alt={cat.name} 
+          width={cat.imageTheme.width} 
+          height={cat.imageTheme.height}
+        />
+      </div>
+    </>
   )
 }
 
