993f73a42cc09a7934e46b97c75a8e8a67ef195f
minion-K <rnjsalsgus06@naver.com>
2025-10-30 11:42:37 +0900

    1030 React Rendering

---
+++ f-iot-react/src/pages/a_basic/I_Rendering.tsx
@@ -0,0 +1,136 @@
+import React from 'react'
+
+//! 렌더링(Rendering)
+// : 작성한 코드를 화면에 출력하는 기능
+// - React가 코드(JSX/TSX)를 실제 브라우저 화면(UI)으로 바꾸는 과정
+
+// cf) React는 데이터(state, props)가 변경되면 필요한 부분만 다시 렌더링
+//     > 화면 전체가 아니라 변경된 컴포넌트만 효율적으로 갱신 (재렌더링)
+
+interface ItemType {
+  // 여행 짐 싸기
+  // : 짐 항목의 이름, 준비 완료 여부
+  name: string;
+  isPacked: boolean;
+}
+
+//! 자식 컴포넌트
+function Item({name, isPacked}: ItemType) {
+  //? 조건부 렌더링
+  // : 조건에 따라 UI를 다르게 보여주는 방법
+  // - if문: 가장 명확, JSX 안에서 사용 불가 (return 위에서 처리)
+  // - 삼항 연산자: JSX 안에서 표현 가능, 한 줄로 간결하게 작성
+  // - 논리 연산자: "조건이 참인 경우에만 and", "조건이 거짓인 경우에만 and" 사용
+
+  //& 1. if 조건문을 사용한 조건부 렌더링
+  //? react의 JSX는 괄호가 문법적 요소로 사용 - return 문 위에서 작성
+  // if(isPacked) {
+  //   return (
+  //     <li>{name} ✔️</li>
+  //   )
+  // } else {
+  //   return (
+  //     <li>{name}</li>
+  //   )
+  // }
+
+  //& 2. 삼항 연산자를 사용한 조건부 렌더링
+  // 조건 ? 참: 거짓
+  // return (
+  //   <li>{isPacked ? name + '✔️' : name}</li>
+  // )
+
+  //& 3. 논리 연산자를 사용한 조건부 렌더링
+  return (
+    <li>
+      {/* 
+        == 논리 연산자 A && B
+        : 모든 값이 true여야 true값 반환
+        - A가 false면 B값 출력 X
+        - A가 true면 B값 출력
+      */}
+      {name}{isPacked && '✔️'}
+    </li>
+  )
+}
+
+function I_Rendering() {
+  //! 배열 렌더링
+  // : 배열 렌더링 시 map() 메서드를 사용
+  // - react는 return문 안에서 JSX 요소들의 배열을 렌더링
+  // > 순회한 후 데이터 값이 있어야 element에 데이터 전달
+
+  // cf) forEach()는 단순 반복 실행만 하고 return 값이 없음
+
+  const people = ['홍길동', '김철수', '이영희', '김유리', '이지훈'];
+
+  const peopleDescription = [
+    {
+      id: 0,
+      name: '홍길동',
+      job: '원장'
+    },
+    {
+      id: 1,
+      name: '김철수',
+      job: '멘토'
+    },
+    {
+      id: 2,
+      name: '조승범',
+      job: '강사'
+    },
+    {
+      id: 3,
+      name: '이승아',
+      job: '강사'
+    },
+    {
+      id: 4,
+      name: '김소빈',
+      job: '부원장'
+    }
+  ];
+
+  //& map 콜백함수를 사용한 배열 렌더링
+  // : 요소 개수 변화 X
+  // - 전체 내용 렌더링
+  const listItems = people.map((person, index) => {
+    return <li key={index}>{person}</li>
+  });
+
+  //& filter 콜백함수를 사용한 배열 렌더링
+  // : 요소 개수 변화 O
+  // - 조건부에 따른 렌더링
+  const businessPeople = peopleDescription.filter(
+    (person) => person.job === '강사'
+  );
+
+  const businessPeopleRender = businessPeople.map(
+    person => <li key={person.id}>{person.name}</li>
+  )
+
+  //! === React 렌더링 시 key 속성 === //
+  // : React에서 배열의 각 항목을 식별하고 성능을 최적화하기 위해 사용
+  // - map()을 통해 여러 요소를 렌더링할 때, 
+  //    key는 React가 어떤 항목이 변경, 추가 또는 삭제 되었는지 파악하는 용도로 사용
+  //? 같은 배열 내에서 유일 (데이터의 PK값을 주로 사용)한 값을 사용
+
+  return (
+    <div>
+      <p>여행용 짐 목록</p>
+      <Item name='과자' isPacked={true}/>
+      <Item name='음료수' isPacked={false}/>
+
+      <hr />
+      <p>map을 사용한 전체 리스트 렌더링</p>
+      <ul>{listItems}</ul>
+
+      <hr />
+      <p>filter를 사용한 조건부 렌더링</p>
+      <ul>{businessPeopleRender}</ul>
+    </div>
+  )
+}
+
+export default I_Rendering
--- f-iot-react/src/pages/a_basic/index.tsx
+++ f-iot-react/src/pages/a_basic/index.tsx
@@ -8,6 +8,7 @@ import E_JSX from './E_JSX'
 import F_Review, { ExampleComponenet } from './F_Review'
 import G_Props from './G_Props'
 import H_Props from './H_Props'
+import I_Rendering from './I_Rendering'
 
 const h2Style ={
   backgroundColor: 'black',
@@ -47,6 +48,9 @@ function Index() {
       <h2 style={h2Style}>4. Props: 리액트의 데이터 전달 (부모/자식)</h2>
       <G_Props />
       <H_Props />
+
+      <h2 style={h2Style}>5. Rendering: 조건부 렌더링</h2>
+      <I_Rendering />
     </div>
     
   )
