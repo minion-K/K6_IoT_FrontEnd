91b2fe43920d93fbb8749efb3bc8d2eeabf836bd
minion-K <rnjsalsgus06@naver.com>
2025-11-03 15:26:00 +0900

    1103 React SignUp Example

--- f-iot-react/src/pages/c_hooks/State04.tsx
+++ f-iot-react/src/pages/c_hooks/State04.tsx
@@ -47,14 +47,67 @@ function State04() {
 
   //& === Event Handler ===
   //% 1) 입력 필드의 변경을 감지하는 이벤트 핸들러
-  const handleSignUpInputChange = () => {
+  const handleSignUpInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target;
 
+    setFormData({
+      ...formData,
+      [name]: value
+    })
   }
 
   //% 2) 폼 제출 이벤트를 처리하는 이벤트 핸들러
   // : 제출 전 각 입력 요소에 대한 유효성 검사
-  const handleSignUpSubmit = () => {
+  const handleSignUpSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
 
+    //? 임시 오류 메시지 객체
+    let tempError = {
+      id: '',
+      password: '',
+      confirmPassword: '',
+      email: ''
+    }
+
+    //? 폼의 유효성 성태를 추적하는 변수
+    let isValid = true; // 하나라도 유효하지 않으면 false로 지정
+    
+    //? -- 필수 입력값 제크 --
+    if(!id.trim()) {
+      tempError.id = '아이디를 입력해주세요.';
+      isValid = false;
+    }
+
+    if(!password.trim()) {
+      tempError.password = '비밀번호를 입력해주세요.';
+      isValid = false;
+    }
+
+    if(!confirmPassword.trim()) {
+      tempError.confirmPassword = '비밀번호 확인을 입력해주세요.';
+      isValid = false;
+    }
+
+    if(!email.trim()) {
+      tempError.email = '이메일을 입력해주세요.'
+      isValid = false;
+    }
+
+    // -- 비밀번호 일치 여부 검사 --
+    if(password && confirmPassword && password != confirmPassword) {
+      tempError.confirmPassword = '비밀번호가 일치하지 않습니다.';
+      isValid = false;
+    }
+
+    setError(tempError);
+
+    if(isValid) {
+      // 모든 유효성 검사 성공
+      console.log('회원가입 데이터: ', formData)
+      alert(`회원가입이 완료되었습니다. ${id}님`)
+
+      setFormData(initialFormData);
+    }
   }
 
   return (
@@ -67,17 +120,80 @@ function State04() {
       }}
     >
       <h3>회원 가입</h3>
-      <form onSubmit={handleSignUpSubmit}>
+      <form onSubmit={handleSignUpSubmit} style={{display: 'flex', flexDirection: 'column', gap: '10px'}}>
         <div>
-          <label>
+          <label style={{
+            width: '300px',
+            margin: '0 auto',
+            display: 'flex',
+            justifyContent: 'space-between'
+          }}>
             아이디
             <input 
               type="text" 
               name='id'
-              value={id}
+              value={formData.id}
               onChange={handleSignUpInputChange}
             />
           </label>
+          {/* error에 객체에 해당 속성이 있으면 error 출력 */}
+          {/* 
+            p태그 외부 () 생략 가능
+            : JSX 안에서 한 줄짜리 표현식은 ()로 감싸지 않아도 됨
+          */}
+          {error.id && <p style={{color: 'red'}}>{error.id}</p>}
+        </div>
+        <div>
+          <label style={{
+            width: '300px',
+            margin: '0 auto',
+            display: 'flex',
+            justifyContent: 'space-between'
+          }}>
+            비밀번호
+            <input 
+              type="text" 
+              name='password'
+              value={formData.password}
+              onChange={handleSignUpInputChange}
+            />
+          </label>
+          {error.password && <p style={{color: 'red'}}>{error.password}</p>}
+        </div>
+        <div>
+          <label style={{
+            width: '300px',
+            margin: '0 auto',
+            display: 'flex',
+            justifyContent: 'space-between'
+          }}>
+            비밀번호 확인
+            <input 
+              type="text" 
+              name='confirmPassword'
+              value={formData.confirmPassword}
+              onChange={handleSignUpInputChange}
+            />
+          </label>
+          {error.confirmPassword && <p style={{color: 'red'}}>{error.confirmPassword}</p>}
+        </div>
+        <div>
+          <label style={{
+            width: '300px',
+            margin: '0 auto',
+            display: 'flex',
+            justifyContent: 'space-between'
+          }}>
+            이메일
+            <input 
+              type="email" 
+              name='email'
+              value={formData.email}
+              onChange={handleSignUpInputChange}
+            />
+          </label>
+          {error.email && <p style={{color: 'red'}}>{error.email}</p>}
+          <button type='submit' style={{marginTop: '10px'}}>회원가입</button>
         </div>
       </form>
     </div>
