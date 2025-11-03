5807ac60c01b27976121218e22d85d6ae9dfdab5
minion-K <rnjsalsgus06@naver.com>
2025-11-03 10:22:39 +0900

    React Post Practice

--- f-iot-react/src/App.tsx
+++ f-iot-react/src/App.tsx
@@ -4,6 +4,7 @@ import Basic from './pages/a_basic'
 import RoutePages from '@/pages/b_Route'
 import Navibar from './components/Navibar'
 import PostList from './pages/a_basic/PostList'
+import PostDetail from './components/PostDetail'
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path='/route/*' element={<RoutePages />}/>
 
         <Route path='/practice/post' element={<PostList />} />
+        <Route path='/practice/post/:id' element={< PostDetail/>} />
       </Routes>
     </>
     
--- f-iot-react/src/components/PostDetail.tsx
+++ f-iot-react/src/components/PostDetail.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function PostDetail() {
+  //? useParams 호출 시 URL의 변수들을 객체로 반환
+  const params = useParams();
+  console.log(params);
+
   return (
-    <div>PostDetail</div>
+    <div>
+      <h1>UserID: {params.id}</h1>
+    </div>
   )
 }
 
