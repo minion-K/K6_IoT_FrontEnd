49f5bd38e82c29e226aabdd165a7c816b38549d7
minion-K <rnjsalsgus06@naver.com>
2025-11-14 12:02:36 +0900

    React Emotion & Chart

--- g-css-react/src/App.tsx
+++ g-css-react/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import { useState } from 'react'
 import { darkTheme, lightTheme } from './styles/theme';
 import { GlobalStyles } from './styles/global';
+import Dashboard from './components/Dashboard';
 
 function App() {
   const [isDark, setIsDark] = useState<boolean>(false);
@@ -13,7 +14,8 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles theme={theme}/>
-      <h1>G-CSS-REACT 프로젝트</h1>
+      {/* <h1>G-CSS-REACT 프로젝트</h1> */}
+      <Dashboard toggleTheme={toggleTheme}/>
     </ThemeProvider>
   )
 }
