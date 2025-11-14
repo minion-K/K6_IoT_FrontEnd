61ac6269b9dd45c8d62c450490bdedcb551bdc15
minion-K <rnjsalsgus06@naver.com>
2025-11-14 09:52:30 +0900

    1114 create project g-css-react & settings

---
+++ g-css-react/src/App.tsx
@@ -0,0 +1,21 @@
+import { ThemeProvider } from '@emotion/react'
+import './App.css'
+import { useState } from 'react'
+import { darkTheme, lightTheme } from './styles/theme';
+import { GlobalStyles } from './styles/global';
+
+function App() {
+  const [isDark, setIsDark] = useState<boolean>(false);
+  const toggleTheme = () => setIsDark(prev => !prev);
+
+  const theme = isDark ? darkTheme : lightTheme;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles theme={theme}/>
+      <h1>G-CSS-REACT 프로젝트</h1>
+    </ThemeProvider>
+  )
+}
+
+export default App
